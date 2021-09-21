9b22a6481123b265a2d9c7a88e7adf6c1dc2d354
hann <hmweeks93@gmail.com>
2021-09-21 11:33:47 -0500

    updated task and category providers

---
+++ src/components/task/TaskProvider.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react"
+
+export const TaskContext = React.createContext()
+
+export const TaskProvider = (props) => {
+    const [ tasks, setTasks ] = useState([])
+
+    const getTasks = () => {
+        return fetch("http://localhost:8000/tasks", {
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("life-admin-token")}`
+            }
+        })
+            .then(response => response.json())
+            .then(setTasks)
+    }
+
+    const getTask = (taskId) => {
+        return fetch(`http://localhost:8000/tasks/${taskId}`, {
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("life-admin-token")}`
+            }
+        }).then(res => res.json())
+    }
+
+    const createTask = (task) => {
+        return fetch("http://127.0.0.1:8000/tasks", {
+            method: "POST",
+            headers: {
+                Authorization: `Token ${localStorage.getItem('life-admin-token')}`,
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+            .then(getTasks)
+    }
+
+    const updateTask = (task) => {
+        return fetch(`http://127.0.0.1:8000/tasks/${task.id}`, {
+            method: "PUT",
+            headers: {
+                Authorization: `Token ${localStorage.getItem('life-admin-token')}`,
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+            .then(getTasks)
+    }
+    
+    
+
+    return (
+        <TaskContext.Provider value={{ tasks, getTasks, createTask, getTask, updateTask }} >
+            { props.children }
+        </TaskContext.Provider>
+    )
+}
